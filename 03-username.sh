8324a21a918188ae989e4827e3b715b04d931d01
Khamruddinshaik <khamruddins18@gmail.com>
2025-01-08 16:40:15 +0530

    practice

---
+++ 03-username.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#This line is commented
+echo "please enter your user name:"
+read USERNAME
+ech0 "$USERNAME"
+echo "please enter your password:"
+READ PASSWORD
---
+++ 03_Args.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#This line is commented
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1 Hi"
+echo "$PERSON2 Hi"
