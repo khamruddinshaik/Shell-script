c558490669a7b81400eb46636e47640e38f3ee3a
Khamruddinshaik <khamruddins18@gmail.com>
2025-01-09 17:54:07 +0530

    practice

--- 06-conditions.sh
+++ 06-conditions.sh
@@ -2,9 +2,9 @@
 
 NUMBER1=$1
 
-if [$NUMBER1 -gt 100]
+if [ $NUMBER1 -gt 100 ]
     then 
-        echo "value is greater than hundred"
+    echo "value is greater than hundred"
 else
     echo "value is less than or equal to hundred"
 fi
