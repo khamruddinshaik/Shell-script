c736677c011041ecc97dcd2164adacfcc92242d0
Khamruddinshaik <khamruddins18@gmail.com>
2025-01-09 18:06:16 +0530

    practice

---
+++ 07-mysql install.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+    then
+    echo " required sudo access to execute the script"
+    exit 1
+fi
+
+dnf list mysql installed
+
+if [ $? -ne 0 ]
+    then
+    dnf install mysql -y
+    if [ $? -ne 0 ]
+        then 
+        echo "Installing mysql.. failure"
+        exit 1
+    else
+        echo "Installing mysql.. success"
+    fi
+else
+    echo "mysql is aldready installed"
+fi
+
+dnf list git installed
+
+if [ $? -ne 0 ]
+    then
+    dnf install git -y
+    if [ $? -ne 0 ]
+        then 
+        echo "Installing git.. failure"
+        exit 1
+    else
+        echo "Installing git.. success"
+    fi
+else
+    echo "git is aldready installed"
+fi
+
+
+
